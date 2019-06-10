90fea074bb78670954052022befc29c6409f8d4a
Tristan Ross <tgr1998@live.com>
2019-06-10 13:39:42 -0700

    Initial Commit

---
+++ caeser.rb
@@ -0,0 +1,9 @@
+class crypto
+    def caeser_cipher(str, num)
+        for x in 0..str.length - 1
+            puts str[x]
+        end
+        puts num
+    end
+    casesar_cipher("testing wow", 0)
+end
